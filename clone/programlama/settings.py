b2bb43905905b9cb49a9ce67ba62e9dd77b3ed51
Veysel imrak <veyselimrak66@gmail.com>
2022-12-20 20:53:41 +0300

    pages klasoru eklendi

--- clone/programlama/settings.py
+++ clone/programlama/settings.py
@@ -31,12 +31,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+#Kayıt sırasına göre çalışır.
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'pages.apps.PagesConfig',
 ]
 
 MIDDLEWARE = [
