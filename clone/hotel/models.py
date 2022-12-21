8c764d51c0270dfa3e584ea8a00322010b188dc8
Veysel imrak <veyselimrak66@gmail.com>
2022-12-21 22:44:14 +0300

    Ana Model Olusturuldu

---
+++ clone/hotel/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+class Hotel(model.Model):
+    STATUS = (
+        ('True', 'Evet'),
+        ('False', 'Hayir'),
+    )
+    title = models.CharField(max_length=50)
+    keywords = models.CharField(blank=True, max_length=200)
+    description = models.CharField(blank=True, max_length=400)
+    image = models.ImageField(blank=True, upload_to='images/')
+    status = models.CharField(max_length=10, choices=STATUS)
+    detail = models.TextField()
+    star = models.FloatField()
+    address = models.CharField(blank=True, max_length=150)
+    phone = models.CharField(blank=True, max_length=15)
+    fax = models.CharField(blank=True, max_length=15)
+    email = models.CharField(blank=True, max_length=50)
+    city = models.CharField(blank=True, max_length=50)
+    country = models.CharField(blank=True, max_length=50)
+    location = models.CharField(blank=True, max_length=150)
+    categoryid = ForeignKey('self', blank = True, null = True, related_name = 'children', on_delete = models.CASCADE)
+    userid = ForeignKey('self', blank = True, null = True, related_name = 'children', on_delete = models.CASCADE)
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+
+
+# Create your models here.
--- clone/programlama/settings.py
+++ clone/programlama/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pages.apps.PagesConfig',
+    'hotel.apps.HotelConfig',
+
 ]
 
 MIDDLEWARE = [
