53002d6ac579c2a63f6a9be19ad0d102f3984dc9
Veysel imrak <veyselimrak66@gmail.com>
2022-12-21 19:45:33 +0300

    about eklendi

--- clone/pages/urls.py
+++ clone/pages/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('about/', views.about, name="about"),
+
     # path(route, views, opt(kısayol ismi))
 
 ]
--- clone/pages/views.py
+++ clone/pages/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'index.html')
 
+def about(request):
+    return render(request, 'about.html')
+
 # Create your views here.
